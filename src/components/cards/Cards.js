1f27d9dbfa165fa22a61dc0e1d7a510bf0ba059d
richie <rickromero99@gmail.com>
2019-07-26 11:36:57 -0700

    updated 7/2019

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './card.css';
 
 const Cards = props => (
+    // <div className=" col-sm-2 col-md-2 col-lg-2 ">
     <div className="col-2">
         <div class="portrait square landscape">
         <img className="images" alt={props.name} src={props.image}
