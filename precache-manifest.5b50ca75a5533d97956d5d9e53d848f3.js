dccca03a2726174dc8e6a346b52f59ae6b515e48
richie <rickromero99@gmail.com>
2019-07-26 20:43:32 -0700

    Updates

--- precache-manifest.5b50ca75a5533d97956d5d9e53d848f3.js
+++ precache-manifest.5b50ca75a5533d97956d5d9e53d848f3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "465aba87c6197c4077cee06025e74657",
+    "revision": "522e73795fb0d7c9ead18882f8a9483d",
     "url": "/clicky_game/index.html"
   },
   {
-    "revision": "0a8c629542b00e2ceccc",
-    "url": "/clicky_game/static/css/main.75b4914f.chunk.css"
+    "revision": "7079cccc81c71df8d06e",
+    "url": "/clicky_game/static/css/main.bf53a0d0.chunk.css"
   },
   {
     "revision": "21e0455ab157e3c726f6",
     "url": "/clicky_game/static/js/2.4282bdd0.chunk.js"
   },
   {
-    "revision": "0a8c629542b00e2ceccc",
-    "url": "/clicky_game/static/js/main.bb6aeb47.chunk.js"
+    "revision": "7079cccc81c71df8d06e",
+    "url": "/clicky_game/static/js/main.69da54e8.chunk.js"
   },
   {
     "revision": "ef6ad64b0df3315f3b6a",
